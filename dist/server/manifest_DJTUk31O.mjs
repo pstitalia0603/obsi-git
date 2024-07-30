f2416466dfbd82dd6dbf079faf9fbf08fae428ee
pstitalia0603 <paula@fragomeni.us>
2024-07-30 16:36:53 -0400

    dist

---
+++ dist/server/manifest_DJTUk31O.mjs
@@ -0,0 +1,6 @@
+import '@astrojs/internal-helpers/path';
+import 'cookie';
+export { m as manifest } from './chunks/_@astrojs-manifest_CvXDJqSH.mjs';
+import 'kleur/colors';
+import './chunks/astro/server_BS5YYp3C.mjs';
+import 'clsx';
